ba8509b117ff6eda769f52dab1698a7c96a027f0
Cristian <u201810994@upc.edu.pe>
2022-03-30 14:47:12 -0500

    Segundo Upload formulario reactivo registro proyecto

--- src/app/access/apartment/apartment-new/apartment-new.component.ts
+++ src/app/access/apartment/apartment-new/apartment-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-apartment-new',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApartmentNewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+ 
 
   ngOnInit(): void {
   }
--- src/app/access/project/project-new/project-new.component.ts
+++ src/app/access/project/project-new/project-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-project-new',
@@ -7,7 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectNewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  registerProjectForm = this.fb.group({
+    nombre: ['', Validators.required], 
+    ubicacion: ['', Validators.required],
+    precioBase: ['', Validators.required]
+  });
+
+  __onSubmit(){
+    if (this.registerProjectForm.valid) {
+      console.log(this.registerProjectForm.value)
+    } else {
+      Swal.fire("Formulario no Valido")
+    }
+  }
 
   ngOnInit(): void {
   }
